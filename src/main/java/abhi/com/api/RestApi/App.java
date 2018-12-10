f8544dd23357ba03670f00912024cf1347195138
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-12-10 22:08:06 +0530

    Initial Commit

---
+++ src/main/java/abhi/com/api/RestApi/App.java
@@ -0,0 +1,13 @@
+package abhi.com.api.RestApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
