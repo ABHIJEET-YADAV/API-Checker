c0078bf32fdd1db0a2d6cf69faec532639735816
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-12-12 22:41:59 +0530

    Test get request

--- src/test/java/com/qa/tests/GetAPITest.java
+++ src/test/java/com/qa/tests/GetAPITest.java
@@ -1,9 +1,32 @@
 package com.qa.tests;
 
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class GetAPITest {
-  @Test
-  public void f() {
-  }
+import com.qa.base.Base;
+import com.qa.client.RestClient;
+
+public class GetAPITest extends Base{ 
+	
+	Base testBase;
+	String serviceUrl;
+	String apiUrl;
+	String url;
+	RestClient restClient;
+	CloseableHttpResponse closebaleHttpResponse;
+	
+	@BeforeMethod
+	public void setUp() throws ClientProtocolException, IOException{
+		testBase = new Base();
+		serviceUrl = prop.getProperty("URL");
+		apiUrl = prop.getProperty("serviceURL");
+		//https://reqres.in/api/users
+		
+		url = serviceUrl + apiUrl;
+		
+	}
 }
