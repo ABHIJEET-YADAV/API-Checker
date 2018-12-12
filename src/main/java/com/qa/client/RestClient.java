52fc8a00b96bd56784c6388a04e5bf3a5591c99c
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-12-12 21:26:49 +0530

    Get Request

--- src/main/java/com/qa/client/RestClient.java
+++ src/main/java/com/qa/client/RestClient.java
@@ -1,5 +1,36 @@
 package com.qa.client;
 
-public class RestClient {
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class RestClient {
+	    // GET Method without Headers:
+		public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException{
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpget = new HttpGet(url); //http get request
+		CloseableHttpResponse closebaleHttpResponse =  httpClient.execute(httpget); //hit the GET URL
+		
+		return closebaleHttpResponse;
+			
+		}
+		
+		// GET Method with Headers:
+			public CloseableHttpResponse get(String url, HashMap<String, String> headerMap) throws ClientProtocolException, IOException{
+			CloseableHttpClient httpClient = HttpClients.createDefault();
+			HttpGet httpget = new HttpGet(url); //http get request
+			
+			for(Map.Entry<String,String> entry : headerMap.entrySet()){
+				httpget.addHeader(entry.getKey(), entry.getValue());
+			}
+			CloseableHttpResponse closebaleHttpResponse =  httpClient.execute(httpget); //hit the GET URL
+			return closebaleHttpResponse;
+				
+			}
 }
