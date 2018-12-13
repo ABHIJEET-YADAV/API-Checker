bb0a8be3f3a86bcd665458ae4d6de05d28ad6321
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-12-13 13:54:40 +0530

    Status check

--- src/main/java/com/qa/base/Base.java
+++ src/main/java/com/qa/base/Base.java
@@ -9,6 +9,12 @@ public class Base {
 	
 	public Properties prop;
 	
+	public int RESPONSE_STATUS_CODE_200 = 200;
+	public int RESPONSE_STATUS_CODE_500 = 500;
+	public int RESPONSE_STATUS_CODE_400 = 400;
+	public int RESPONSE_STATUS_CODE_401 = 401;
+	public int RESPONSE_STATUS_CODE_201 = 201;
+	
 	public void TestBase(){
 		try {
 			prop = new Properties();
--- src/test/java/com/qa/tests/GetAPITest.java
+++ src/test/java/com/qa/tests/GetAPITest.java
@@ -1,17 +1,21 @@
 package com.qa.tests;
 
 import java.io.IOException;
+import java.util.HashMap;
 
+import org.apache.http.Header;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.qa.base.Base;
 import com.qa.client.RestClient;
 
-public class GetAPITest extends Base{ 
-	
+public class GetAPITest extends Base{
 	Base testBase;
 	String serviceUrl;
 	String apiUrl;
@@ -19,14 +23,44 @@ public class GetAPITest extends Base{
 	RestClient restClient;
 	CloseableHttpResponse closebaleHttpResponse;
 	
+	
 	@BeforeMethod
 	public void setUp() throws ClientProtocolException, IOException{
 		testBase = new Base();
 		serviceUrl = prop.getProperty("URL");
 		apiUrl = prop.getProperty("serviceURL");
-		//https://reqres.in/api/users
-		
 		url = serviceUrl + apiUrl;
+		
+	}
+	
+	
+	
+	@Test(priority=1)
+	public void getAPITestWithoutHeaders() throws ClientProtocolException, IOException{
+		restClient = new RestClient();
+		closebaleHttpResponse = restClient.get(url);
+		
+		//a. Status Code:
+		int statusCode = closebaleHttpResponse.getStatusLine().getStatusCode();
+		System.out.println("Status Code--->"+ statusCode);
+		
+		Assert.assertEquals(statusCode, RESPONSE_STATUS_CODE_200, "Status code is not 200");
+
+		//b. Json String:
+		String responseString = EntityUtils.toString(closebaleHttpResponse.getEntity(), "UTF-8");
+		
+		JSONObject responseJson = new JSONObject(responseString);
+		System.out.println("Response JSON from API---> "+ responseJson);
+		
+		//c. All Headers
+		Header[] headersArray =  closebaleHttpResponse.getAllHeaders();
+		HashMap<String, String> allHeaders = new HashMap<String, String>();	
+		for(Header header : headersArray){
+			allHeaders.put(header.getName(), header.getValue());
+		}	
+		System.out.println("Headers Array-->"+allHeaders);
+		
+		
 		
 	}
 }
